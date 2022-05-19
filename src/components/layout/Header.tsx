036dbc670636546400103fb3acbf0265e0c140a7
vanillovin <vanillovin@gmail.com>
2022-05-20 00:13:37 +0900

    feat: Header 컴포넌트에 시계 추가 & 스타일

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 
 const Header = () => {
   const location = useLocation();
+  const [clock, setClock] = useState('');
+
+  function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    setClock(
+      `${hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes
+      }:${seconds < 10 ? `0${seconds}` : seconds}`
+    );
+  }
+
+  useEffect(() => {
+    const intervalId = setInterval(getTime, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div
-      className={`lg:text-lg md:text-base w-full h-20 flex flex-row items-center justify-between 
-                  px-5 tablet:px-10 bg-blue-100`}
+      className={`text-xs tablet:text-base w-full h-14 tablet:h-20 flex flex-row items-center justify-between 
+                  px-5 tablet:px-10 bg-blue-100 select-none text-gray-600 font-bold`}
     >
-      <div>
+      <div className="w-1/3">
         <Link to="/">Logo</Link>
       </div>
-      <ul className="flex items-center">
+      <div className="w-1/3 text-center flex items-center justify-center">
+        <p className="font-bold rounded-3xl border-2 border-white py-1 px-2 text:base tablet:text-xl">
+          {clock}
+        </p>
+      </div>
+      <ul className="flex items-center w-1/3 justify-end">
         {[
           ['/diary', 'Diary'],
           ['/calender', 'Calender'],
         ].map(([path, title]) => (
           <li
             key={title}
-            className={`ml-6 hover:opacity-70 ${
-              location.pathname === path && 'font-bold text-blue-400'
+            className={`ml-2 tablet:ml-6 hover:opacity-70 ${
+              location.pathname.includes(path) && 'text-blue-400'
             }`}
           >
             <Link to={path}>{title}</Link>
