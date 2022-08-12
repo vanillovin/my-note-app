0a911782b8a7a13beab2eb0b2b5011ed45086977
vanillovin <vanillovin@gmail.com>
2022-08-12 13:04:12 +0900

    폴더 위치 변경

--- src/hooks/useModal.ts
+++ src/hooks/useModal.ts
@@ -2,11 +2,8 @@ import { useState } from 'react';
 
 const useModal = () => {
   const [isShowing, setIsShowing] = useState(false);
-
   const openModal = () => setIsShowing(true);
-
   const closeModal = () => setIsShowing(false);
-
   return { isShowing, openModal, closeModal };
 };
 
