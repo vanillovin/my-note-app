c43d7236be513c73f0d019ae862f04baae1edaeb
vanillovin <vanillovin@gmail.com>
2023-05-18 14:47:48 +0900

    feat: 날씨 api를 불러올 때 로딩 메시지 추가

--- src/components/weather/Weather.tsx
+++ src/components/weather/Weather.tsx
@@ -42,7 +42,10 @@ function Weather() {
         const url = `https://api.openweathermap.org/data/2.5/weather?lat=${res.latitude}&lon=${res.longitude}&appid=${API_KEY}&units=metric`;
         fetch(url)
           .then((res) => res.json())
-          .then((data) => setWeatherData(data));
+          .then((data) => {
+            // console.log(data);
+            setWeatherData(data);
+          });
       })
       .catch((err) => alert(err));
   }, []);
@@ -54,21 +57,23 @@ function Weather() {
     >
       <TodoProgress />
       <div className="flex items-center w-1/2 py-2 px-1 tablet:px-4 justify-center">
-        <div>
-          <p className="text-sm tablet:text-lg font-bold">
-            {weatherData?.name !== '' && `${weatherData?.name},`}{' '}
-            {weatherData?.sys.country}
-          </p>
-          <p className="text-sm tablet:text-lg font-bold">{`${weatherData?.main?.temp.toFixed()}℃`}</p>
-          <p className="text-xs tablet:text-sm opacity-60">
-            {weatherData?.weather[0].description}
-          </p>
-        </div>
-        <img
-          className="w-16 tablet:w-32 tablet:ml-3"
-          alt={weatherData?.weather[0].main}
-          src={`http://openweathermap.org/img/wn/${weatherData?.weather[0].icon}@4x.png`}
-        />
+        {weatherData ? (
+          <>
+            <div>
+              <p className="text-sm tablet:text-lg font-bold">{`${weatherData.main.temp.toFixed()}℃`}</p>
+              <p className="text-xs tablet:text-sm opacity-60">
+                {weatherData.weather[0].description}
+              </p>
+            </div>
+            <img
+              className="w-16 tablet:w-32 tablet:ml-3"
+              alt={weatherData.weather[0].main}
+              src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}
+            />
+          </>
+        ) : (
+          <div>날씨 정보를 불러오고 있습니다 🍀</div>
+        )}
       </div>
     </div>
   );
